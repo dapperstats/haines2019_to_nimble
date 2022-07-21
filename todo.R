5289c5582fcdc1368540fb522f3ab4bfeb5daf9e
Juniper Simonis <dapperstats@gmail.com>
2022-07-20 23:32:12 -0700

    tidy

--- todo.R
+++ todo.R
@@ -1,21 +1,5 @@
 tasks
 
- - add poisson versions of each model
-  
- - flex the probabilities to match the existing model
-  - does not need to condition on detection!
-  
- - look into parameter definition for flexible model structure
-  - linear predictors
+ - flex the probabilities to match the mechanistic model
 
-
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-- working in carcass model approach expansion
-  - need to write out the full likelihood equations!
-  - work from them into the code, check via existing models!
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ - linear predictors algebraic setup?
